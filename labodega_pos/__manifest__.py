28c1cbd98c8cd1a9b1993629b09eb7467d34f70a
Shelton <sheltonfreddy141@gmail.com>
2026-01-01 08:58:49 -0500

    pos receipt changes

--- labodega_pos/__manifest__.py
+++ labodega_pos/__manifest__.py
@@ -11,6 +11,8 @@
         "point_of_sale._assets_pos": [
             "labodega_pos/static/src/js/magellan_config.js",
             "labodega_pos/static/src/js/magellan_scale_service.js",
+            "labodega_pos/static/src/css/compact_receipt.css",
+            "labodega_pos/static/src/xml/compact_receipt.xml",
         ],
     },
     "installable": True,
